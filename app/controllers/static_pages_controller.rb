4e2d2cd5c047bb9acfa4c31b6eabedd17542111b
Michael <michaelroe1@berkeley.edu>
2015-04-24 14:18:09 -0700

    added map button to location page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,8 +6,6 @@ class StaticPagesController < ApplicationController
 		state = params[:name]
 		@l = Location.find_by_name(state)
 		@month = Date::MONTHNAMES[Date.today.month]
-		#in location.rb use getproduce method
 		@p = @l.get_produce(@month)
-		#redirect_to index_path(@p)
 	end
 end
