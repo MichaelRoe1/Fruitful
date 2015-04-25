ef1c7db2ea8a281a76c2e5b4d53986a5b37cfd46
Daniel Goldberg <dgoldberg25@gmail.com>
2015-04-25 09:55:01 -0700

    heroku views fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,9 @@ class StaticPagesController < ApplicationController
 	def location
 		state = params[:name]
 		@l = Location.find_by_name(state)
+		if @l == nil
+			flash.now[:error] = "Please enter a State."
+		end
 		@month = Date::MONTHNAMES[Date.today.month]
 		@p = @l.get_produce(@month)
 	end
